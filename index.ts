a2234e722f99e4ddd4a727c89045a91f83b48c71
Woody Lewis <woody@woodylewis.com>
2020-02-23 11:33:16 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,139 @@
+import {
+  ErrorHandler,
+  HandlerInput,
+  RequestHandler,
+  SkillBuilders
+} from "ask-sdk-core";
+import { Response, SessionEndedRequest } from "ask-sdk-model";
+
+import { Context } from "aws-lambda";
+import axios from "axios";
+
+const LaunchRequestHandler: RequestHandler = {
+  canHandle(HandlerInput: HandlerInput): boolean {
+    return HandlerInput.requestEnvelope.request.type === "LaunchRequest";
+  },
+  handle(HandlerInput: HandlerInput): Response {
+    const speechText = "Welcome to the Smart Narrative Network";
+
+    return HandlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .getResponse();
+  }
+};
+
+const HelloWorldIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "Smart Narrative Network!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .getResponse();
+  }
+};
+
+const HelpIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "You can say hello to me!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .getResponse();
+  }
+};
+
+const CancelAndStopIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name ===
+        "AMAZON.CancelIntent" ||
+        handlerInput.requestEnvelope.request.intent.name ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "Goodbye!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+  },
+  handle(handlerInput: HandlerInput): Response {
+    console.log(
+      `Session ended with reason: ${
+        (handlerInput.requestEnvelope.request as SessionEndedRequest).reason
+      }`
+    );
+
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler: ErrorHandler = {
+  canHandle(handlerInput: HandlerInput, error: Error): boolean {
+    return true;
+  },
+  handle(handlerInput: HandlerInput, error: Error): Response {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak("Sorry, I can't understand the command. Please say again.")
+      .reprompt("Sorry, I can't understand the command. Please say again.")
+      .getResponse();
+  }
+};
+
+let skill: any;
+
+exports.handler = async (event: any, context: Context) => {
+  console.log("HERE=======>");
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = SkillBuilders.custom()
+      .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
+      )
+      .addErrorHandlers(ErrorHandler)
+      .create();
+  }
+
+  const fetch = await axios.get(
+    "https://k8p9i0etci.execute-api.us-east-1.amazonaws.com/prod/"
+  );
+  console.log(" FETCHED DATA - " + fetch.data);
+
+  const response = await skill.invoke(event, context);
+  console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+  return response;
+};
